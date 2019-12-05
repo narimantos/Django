9fa149fae4c471591c2b0d909561c6acecca5c1b
narimantos <narimantos@hotmail.com>
2019-12-06 00:50:24 +0100

    pizza toegevoegd aan installed_apps in setting.py en makemigtations && migrate uitgevoerd

--- pizza/models.py
+++ pizza/models.py
@@ -7,3 +7,4 @@ from django.db import models
 class Pizza(models.Model):
     title = models.CharField(max_length=120)
     description = models.CharField(max_length=240)
+    size = models.CharField(max_length=240)
