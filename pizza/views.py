5ba2dd2313658c0dc8a999b526fca401c31fbc2c
narimantos <narimantos@hotmail.com>
2019-12-06 02:19:45 +0100

    kapoet

--- pizza/views.py
+++ pizza/views.py
@@ -3,4 +3,15 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from django.http import HttpResponse
+from .models import Pizza
+def index(request, pid):
+    pizza = Pizza.objects.get(id=pid)
+    return HttpResponse(
+        content={
+            'id': pizza.id,
+            'title': pizza.title,
+            'description': pizza.description,
+        }
+    )
 # Create your views here.
